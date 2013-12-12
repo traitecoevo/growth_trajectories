0fe6cd1099ceb9b1cb38a0285f3b002619b0b610
Daniel Falster <daniel.falster@mq.edu.au>
2013-12-12 17:15:05 +1100

    Add interface for varying height and light environment. Adressess #3.

--- R/tree-fun.R
+++ R/tree-fun.R
@@ -8,7 +8,7 @@ fixed.environment <- function(E, height, n=101, light.env=NULL,
     seed.rain <- rep(seed.rain, n.strategies)
   hh <- seq(0, height, length=n)
   if (is.null(light.env))
-    light.env <- function(x) E
+    light.env <- function(x) rep(E, length.out=length(x))
   ee <- light.env(hh)
   env <- new(Spline)
   env$init(hh, ee)
@@ -24,6 +24,74 @@ fixed.environment <- function(E, height, n=101, light.env=NULL,
   ret
 }
 
+# runs a new plant with given height, environment and strategy
+run_plant <- function(h=0.5, E=1,  strategy = new(Strategy)){
+
+  p <- new(Plant, new(Strategy))
+
+  p$height <- h
+  env <- fixed.environment(E)
+  p$compute_vars_phys(env)
+
+  list(h=h,
+     E = E,
+    vars_size = data.frame(t(p$vars_size)),
+    vars_phys = data.frame(t(p$vars_phys)))
+}
+
+change_with_size <- function(h=seq(0.2, 1, length.out=10), E=1, strategy = new(Strategy)){
+
+  env <- fixed.environment(E)
+  sp <- new(Species, strategy)
+
+  for (i in 1:length(h))
+       sp$add_seeds(1)
+  sp$height <- sort(h, decreasing=TRUE)  # set heights, make sure decreasing
+
+  sp$compute_vars_phys(env)
+  list(h=sp$height,
+    E=E,
+    vars_size = data.frame(t(sp$vars_size)),
+    vars_phys = data.frame(t(sp$vars_phys)))
+
+}
+
+
+# transforms a list of lists into a single list by applying do.call(rbind, ..) to each component of a list
+# assumes all elements of parent list have same structure
+
+merge_list_components <- function(myLists){
+
+  out <- list()
+
+  if(length(myLists) > 0){
+    vars <- names(myLists[[1]])
+    f <- function(v) do.call(rbind, lapply(myLists, "[[", v))
+      out <- structure(lapply(vars, f), names=vars)
+  }
+  out
+}
+
+change_with_light <- function( h=0.2, E=seq(0.1, 1, length.out=20), strategy = new(Strategy)){
+
+  tmp <- lapply(E, function(x) run_plant(h=h, E=x, strategy=strategy))
+
+  merge_list_components(tmp)
+}
+
+# step with ode stepper
+require(deSolve, quietly=TRUE)
+
+step_ode <- function(light.env, plant, timesteps = seq(0, 15, length=51)){
+
+  pars.derivs <- list(plant=p, light.env=env)
+  derivs.d <- function(...) list(derivs(...))
+
+  plant.run <- rk(y, timesteps, derivs.d, pars.derivs,
+                       method=rkMethod("rk45ck"), hini=1/1000, rtol=1,
+                       atol=1)
+
+}
 
 ## Grow the plant in a constant environment
 derivs <- function(t, y, pars) {
--- analysis.R
+++ analysis.R
@@ -1,54 +1,18 @@
 
 source("R/tree-fun.R")
 
+#
 s <- new(Strategy)
-p <- new(Plant, s)
+run_plant(h=0.5, E=1,  strategy =s)
 
-# set height
-h0 <- 10
-p$height <- h0
+x <- change_with_size(h=seq(0.1, 30, by=0.1))
 
-size.p <- p$vars_size
+plot(x[["h"]], x$vars_size[["mass_total"]], type='b')
+plot(x[["h"]], x$vars_phys[["height_growth_rate"]], type='b')
 
-# setup light env
-env <- fixed.environment(1)
-light.env <- attr(env, "light.env")
+x <- change_with_light(E=seq(0.1, 1, length.out=50))
 
-## Compute the physiological variables and retreive them.
-p$compute_vars_phys(env)
-p.phys <- p$vars_phys
-p$vars_phys[["height_growth_rate"]]  #dh/dt
-p$vars_phys[["leaf_fraction"]]  #dml/dmt
-p$vars_phys[["reproduction_fraction"]]  #1-r
-# To do: write functions to access other required elements of growth decomposition, see https://github.com/dfalster/TraitGrowthTrajectories/issues/1
+plot(x[["E"]], x$vars_size[["mass_total"]], type='b')
+plot(x[["E"]], x$vars_phys[["height_growth_rate"]], type='b')
 
-## Compute physiological variables across range of heights for single species
-
-sp <- new(Species, s)
-
-## Add four plants with random sizes.
-for (i in 1:4)
-       sp$add_seeds(1)
-sp$height <- sort(runif(sp$size), decreasing=TRUE)  # assign some random heights
-sp$compute_vars_phys(env)
-
-sp$vars_size  # extract size
-sp$vars_phys  # phys
-
-xs <- data.frame(t(sp$vars_size))
-xp <- data.frame(t(sp$vars_phys))
-
-plot(x[["height"]], x[["mass_total"]], type='b')
-
-plot(xs[["height"]], xp[["height_growth_rate"]], type='b')
-
-
-# step with ode stepper
-pars.derivs <- list(plant=p, light.env=env)
-tt <- seq(0, 15, length=51)
-library(deSolve)
-derivs.d <- function(...) list(derivs(...))
-
-plant.run <- rk(y, tt, derivs.d, pars.derivs,
-                     method=rkMethod("rk45ck"), hini=1/1000, rtol=1,
-                     atol=1)
+#x <- change_with_trait....
