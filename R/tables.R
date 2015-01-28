80b7a6ff09cd40976257980454b6501457971b7b
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-01-28 17:02:57 +1100

    Minor cleanup

--- R/tables.R
+++ R/tables.R
@@ -1,6 +1,9 @@
-
 table_tree_parameters <- function(tree_parameters, dest){
-	x<-xtable(tree_parameters[-c(1,2,4,7,10,21,22,25:31),1:4],hline.after=c(1), align='lp{5cm}lll')
-	y <- print(x,sanitize.text.function=function(str)as.character(str),include.rownames=FALSE,floating=FALSE,print.results = FALSE);
-	cat(y, file =dest)
+  x <- xtable(tree_parameters[-c(1,2,4,7,10,21,22,25:31),1:4],
+              hline.after=c(1),
+              align='lp{5cm}lll')
+  y <- print(x, sanitize.text.function=as.character,
+             include.rownames=FALSE, floating=FALSE,
+             print.results=FALSE)
+  cat(y, file=dest)
 }
