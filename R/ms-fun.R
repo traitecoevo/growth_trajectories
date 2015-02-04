e83c23be4c2de7fd1257c2ab4515d3b6dbdedc1d
Daniel Falster <daniel.falster@mq.edu.au>
2015-02-04 15:51:46 +1100

    shift to using latex

--- R/ms-fun.R
+++ R/ms-fun.R
@@ -1,7 +1,7 @@
 
 pandoc_build <- function(file){
   args <- list('--template=include.tex', '--latex-engine=xelatex')
-  pandoc_convert(basename(file), output= paste0(tools::file_path_sans_ext(basename(file)), ".pdf"), citeproc = TRUE, options = args, verbose = FALSE, wd = dirname(file))
+  pandoc_convert(basename(file), to="latex", output= paste0(tools::file_path_sans_ext(basename(file)), ".tex"), citeproc = FALSE, options = args, verbose = FALSE, wd = dirname(file))
 }
 
 get_nature_csl <- function(dest, url="https://raw.githubusercontent.com/citation-style-language/styles/master/nature.csl"){
@@ -10,4 +10,15 @@ get_nature_csl <- function(dest, url="https://raw.githubusercontent.com/citation
 
 get_amnat_csl <- function(dest, url="https://raw.githubusercontent.com/citation-style-language/styles/master/the-american-naturalist.csl"){
 	cat(getURL(url), file=dest)
+}
+
+tex_2_pdf <- function(texfile){
+
+  filename <- tools::file_path_sans_ext(texfile)
+  system(sprintf("xelatex %s", texfile))
+  system(sprintf("bibtex %s",   filename))
+  system(sprintf("xelatex %s", texfile))
+  system(sprintf("xelatex %s", texfile))
+  aux.files <- paste0(filename, c(".log", ".aux", ".bbl", ".blg"))
+  file.remove(aux.files[file.exists(aux.files)])
 }
