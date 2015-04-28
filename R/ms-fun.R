7904b764bfd6fedf3a3401f70f378e894f61f2aa
Daniel Falster <daniel.falster@mq.edu.au>
2015-04-29 07:00:20 +1000

    expand list of ignore accessory files from latex builds

--- R/ms-fun.R
+++ R/ms-fun.R
@@ -17,7 +17,7 @@ latex_build2 <- function(filename, latex_engine="xelatex"){
 
 remove_aux_files <- function(filename) {
 	aux.files <- paste0(tools::file_path_sans_ext(filename),
-                     c(".log", ".aux", ".bbl", ".blg", ".fls", ".out",
+                     c(".log", ".aux", ".bbl", ".blg", ".fls", ".out", ".snm", ".nav", ".toc",
                       ".fdb_latexmk"))
   file.remove(aux.files[file.exists(aux.files)])
 }
