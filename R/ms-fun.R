a1637e7861273ba1e8d47d69288b901ff462320f
Daniel Falster <daniel.falster@mq.edu.au>
2015-04-29 06:41:10 +1000

    Update build tools

--- R/ms-fun.R
+++ R/ms-fun.R
@@ -1,7 +1,22 @@
-tex_2_pdf <- function(texfile){
+latex_build <- function(filename, latex_engine="xelatex", bibtex=TRUE){
+  # filename <- tools::file_path_sans_ext(filename)
+  build <- sprintf("%s -interaction=nonstopmode %s", latex_engine, filename)
+  system(build)
+  if(bibtex){
+	  system(sprintf("bibtex %s",   tools::file_path_sans_ext(filename)))
+  }
+  system(build)
+  system(build)
+  remove_aux_files(filename)
+}
 
-  system(sprintf('latexmk -pdf -pdflatex="xelatex -interaction=nonstopmode" %s', texfile))
-  aux.files <- paste0(tools::file_path_sans_ext(texfile),
+latex_build2 <- function(filename, latex_engine="xelatex"){
+  system(sprintf('latexmk -pdf -pdflatex="%s -interaction=nonstopmode" %s', latex_engine, filename))
+  remove_aux_files(filename)
+}
+
+remove_aux_files <- function(filename) {
+	aux.files <- paste0(tools::file_path_sans_ext(filename),
                      c(".log", ".aux", ".bbl", ".blg", ".fls", ".out",
                       ".fdb_latexmk"))
   file.remove(aux.files[file.exists(aux.files)])
