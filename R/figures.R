be9cab8496fc0f9c17bf2de5553691ea61150b78
Daniel Falster <daniel.falster@mq.edu.au>
2015-04-29 14:40:26 +1000

    New conceptual figure #37

--- R/figures.R
+++ R/figures.R
@@ -327,3 +327,9 @@ figure_mass_fraction <- function() {
 
   box()
 }
+
+empty_box <- function() {
+  par(oma=c(0,0,0,0), mar=rep(0.1,4))
+  plot(1,1, ann=FALSE, axes=FALSE, type='n')
+  box()
+}
--- R/ms-fun.R
+++ R/ms-fun.R
@@ -1,13 +1,17 @@
-latex_build <- function(filename, latex_engine="xelatex", bibtex=TRUE){
-  # filename <- tools::file_path_sans_ext(filename)
+latex_build <- function(filename, latex_engine="xelatex", bibtex=TRUE, dest_dir = "."){
   build <- sprintf("%s -interaction=nonstopmode %s", latex_engine, filename)
+  basename <- tools::file_path_sans_ext(filename)
   system(build)
   if(bibtex){
-	  system(sprintf("bibtex %s",   tools::file_path_sans_ext(filename)))
+	  system(sprintf("bibtex %s",  basename))
+    system(build)
   }
   system(build)
-  system(build)
   remove_aux_files(filename)
+  if(dest_dir!="."){
+    dir.create(dest_dir, showWarnings = FALSE, recursive = TRUE)
+    system(sprintf("mv %s.pdf %s", basename, dest_dir))
+  }
 }
 
 latex_build2 <- function(filename, latex_engine="xelatex"){
@@ -20,4 +24,16 @@ remove_aux_files <- function(filename) {
                      c(".log", ".aux", ".bbl", ".blg", ".fls", ".out", ".snm", ".nav", ".toc",
                       ".fdb_latexmk"))
   file.remove(aux.files[file.exists(aux.files)])
+}
+
+download_seedling_png <- function(destination_filename) {
+  url <-
+    "http://ian.umces.edu/imagelibrary/albums/userpics/10002/normal_ian-symbol-quercus-spp-oak-seedling.png"
+  download(url, destination_filename, mode="wb")
+}
+
+download_tree_png <- function(destination_filename) {
+  url <-
+    "http://ian.umces.edu/imagelibrary/albums/userpics/12789/normal_ian-symbol-quercus-montana.png"
+  download(url, destination_filename, mode="wb")
 }
